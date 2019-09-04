4ce1b4bb1349d819f67db4f43083d719664a622c
VSD89 <vr_89@mail.ru>
2019-09-04 23:50:11 +0700

    Bon Appetit

---
+++ src/ru/vsd/solutions/bonappetit/Solution.java
@@ -0,0 +1,42 @@
+//Problem: https://www.hackerrank.com/challenges/bon-appetit/problem
+//Java 8
+
+package ru.vsd.solutions.bonappetit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int k, int b) {
+        int count = bill.stream().filter(i -> i != bill.get(k)).reduce(0, Integer::sum) / 2;
+        String output = (count == b) ? "Bon Appetit" : String.valueOf(b - count);
+        System.out.println(output);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
